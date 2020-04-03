9c72d6445713ca606a3d3fcfc588522dd0db5266
Ynyr Llewelyn Williams <ynyrllw@lenovo-thinkpad.lan>
2020-04-03 13:46:31 +0100

    added hello world page

---
+++ al_hello/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class AlHelloConfig(AppConfig):
+    name = 'al_hello'
--- al_proj/wsgi.py
+++ al_proj/wsgi.py
@@ -1,16 +1,16 @@
 """
-WSGI config for myapps project.
+WSGI config for al_proj project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
 """
 
 import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "myapps.settings")
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'al_proj.settings')
 
 application = get_wsgi_application()
