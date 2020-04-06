620c90b8427c03063b50525a2be03ed732e7e1af
Ynyr Llewelyn Williams <ynyrllw@gmail.com>
2020-04-06 18:57:05 +0100

    changed wsgi to hello world

--- al_proj/wsgi.py
+++ al_proj/wsgi.py
@@ -1,16 +1,3 @@
-"""
-WSGI config for al_proj project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
-"""
-
-import os
-
-from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'al_proj.settings')
-
-application = get_wsgi_application()
+def application(env, start_response):
+    start_response('200 OK', [('Content-Type','text/html')])
+    return [b"Hello World"]
